e6fc27d3d97790bc5bc1bef780d89470db62025a
Jacob Latonis <jlatonis@me.com>
2024-11-30 21:16:40 -0800

    day one solved

--- day01/solution.cpp
+++ day01/solution.cpp
@@ -1,20 +1,58 @@
 #include "../utils/utils.h"
 
 void part_one() {
-    for (std::string line : get_input_all_lines("./test-input")) {
-        auto inp =
-            line | std::ranges::views::split(' ') |
-            std::views::transform([](auto v) { return std::string_view(v); }) |
-            std::ranges::to<std::vector>();
+    int total = 0;
+    std::vector<int> fs;
+    std::vector<int> ss;
+    for (std::string line : get_input_all_lines("./input")) {
+        auto input_ints = line | std::views::split(std::string("   ")) |
+                          std::ranges::views::transform([](auto v) {
+                              int i = 0;
+                              std::from_chars(v.data(), v.data() + v.size(), i);
+                              return i;
+                          }) |
+                          std::ranges::to<std::vector>();
+        fs.push_back(input_ints[0]);
+        ss.push_back(input_ints[1]);
     }
+    std::sort(fs.begin(), fs.end());
+    std::sort(ss.begin(), ss.end());
+
+    for (int i = 0; i < ss.size(); i++) {
+        total += std::abs(fs[i] - ss[i]);
+    }
+
+    std::cout << "Total calculation: " << total << "\n";
 }
 
 void part_two() {
-    auto line = get_input_one_line("./test-input");
-    auto inp =
-        line | std::ranges::views::split(' ') |
-        std::views::transform([](auto v) { return std::string_view(v); }) |
-        std::ranges::to<std::vector>();
+    int total = 0;
+    std::vector<int> fs;
+    std::unordered_map<int, int> ss;
+    for (std::string line : get_input_all_lines("./input")) {
+        auto input_ints = line | std::views::split(std::string("   ")) |
+                          std::ranges::views::transform([](auto v) {
+                              int i = 0;
+                              std::from_chars(v.data(), v.data() + v.size(), i);
+                              return i;
+                          }) |
+                          std::ranges::to<std::vector>();
+        fs.push_back(input_ints[0]);
+        if (ss.contains(input_ints[1])) {
+            ss.find(input_ints[1])->second++;
+        } else {
+            ss.insert({input_ints[1], 1});
+        }
+    }
+    std::sort(fs.begin(), fs.end());
+
+    for (int i = 0; i < fs.size(); i++) {
+        if (ss.contains(fs[i])) {
+            total += fs[i] * ss.find(fs[i])->second;
+        }
+    }
+
+    std::cout << "Total calculation similarity: " << total << "\n";
 }
 
 int main() {
--- template/solution.cpp
+++ template/solution.cpp
@@ -6,6 +6,15 @@ void part_one() {
             line | std::ranges::views::split(' ') |
             std::views::transform([](auto v) { return std::string_view(v); }) |
             std::ranges::to<std::vector>();
+
+        auto input_ints = get_input_one_line("./test-input") |
+                          std::views::split('\t') |
+                          std::views::transform([](auto v) {
+                              int i = 0;
+                              std::from_chars(v.data(), v.data() + v.size(), i);
+                              return i;
+                          }) |
+                          std::ranges::to<std::vector>();
     }
 }
 
