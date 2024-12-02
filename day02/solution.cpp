1c3e1679429418f411dcb302ce1b10306d097aad
Jacob Latonis <jlatonis@me.com>
2024-12-02 00:37:34 -0800

    more cleanup

--- day02/solution.cpp
+++ day02/solution.cpp
@@ -1,48 +1,5 @@
 #include "../utils/utils.h"
 
-void part_one() {
-    int total = 0;
-    for (std::string line : get_input_all_lines("./input")) {
-        auto input_ints = line | std::views::split(' ') |
-                          std::views::transform([](auto v) {
-                              int i = 0;
-                              std::from_chars(v.data(), v.data() + v.size(), i);
-                              return i;
-                          }) |
-                          std::ranges::to<std::vector>();
-
-        bool safe = true;
-
-        bool inc = true;
-
-        if (input_ints[0] < input_ints[1]) {
-            inc = false;
-        }
-        for (int i = 0; i < input_ints.size() - 1; i = i + 1) {
-            int diff = std::abs(input_ints[i] - input_ints[i + 1]);
-            if (diff > 3 || diff < 1) {
-                safe = false;
-            }
-
-            if (inc) {
-                if (input_ints[i] < input_ints[i + 1]) {
-                    safe = false;
-                }
-            } else {
-                if (input_ints[i] > input_ints[i + 1]) {
-                    safe = false;
-                }
-            }
-        }
-
-        if (safe) {
-            total += 1;
-        }
-    }
-
-    std::cout << "Total: " << total << "\n";
-}
-
 bool check_inc_dec(std::vector<int> input_ints) {
     // inc check
     int count = 0;
@@ -88,6 +45,25 @@ bool check_vec(std::vector<int> input_ints) {
     }
 
     return false;
+}
+
+void part_one() {
+    int total = 0;
+    for (std::string line : get_input_all_lines("./input")) {
+        auto input_ints = line | std::views::split(' ') |
+                          std::views::transform([](auto v) {
+                              int i = 0;
+                              std::from_chars(v.data(), v.data() + v.size(), i);
+                              return i;
+                          }) |
+                          std::ranges::to<std::vector>();
+
+        if (check_inc_dec(input_ints)) {
+            total += 1;
+        }
+    }
+
+    std::cout << "Total: " << total << "\n";
 }
 
 void part_two() {
