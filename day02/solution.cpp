acdfd8e66ad456c3ce324945218a8619fed750fe
Jacob Latonis <jlatonis@me.com>
2024-12-02 00:35:27 -0800

    small cleanup

--- day02/solution.cpp
+++ day02/solution.cpp
@@ -44,15 +44,13 @@ void part_one() {
 }
 
 bool check_inc_dec(std::vector<int> input_ints) {
+    // inc check
     int count = 0;
 
-    // inc check
     for (int i = 0; i < input_ints.size() - 1; i = i + 1) {
-        if (input_ints[i] < input_ints[i + 1]) {
-            int diff = input_ints[i + 1] - input_ints[i];
-            if (diff >= 1 && diff <= 3) {
-                count += 1;
-            }
+        int diff = input_ints[i + 1] - input_ints[i];
+        if (diff >= 1 && diff <= 3) {
+            count += 1;
         }
     }
 
@@ -60,14 +58,13 @@ bool check_inc_dec(std::vector<int> input_ints) {
         return true;
     }
 
-    count = 0;
     // dec check
+    count = 0;
+
     for (int i = 0; i < input_ints.size() - 1; i = i + 1) {
-        if (input_ints[i] > input_ints[i + 1]) {
-            int diff = input_ints[i] - input_ints[i + 1];
-            if (diff >= 1 && diff <= 3) {
-                count += 1;
-            }
+        int diff = input_ints[i] - input_ints[i + 1];
+        if (diff >= 1 && diff <= 3) {
+            count += 1;
         }
     }
 
