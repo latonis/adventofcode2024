74076b8ca080f8df01c1a2d4e10f26cc2abd7f31
Jacob Latonis <jlatonis@me.com>
2024-12-02 23:20:21 -0700

    day 3 solved

---
+++ day03/solution.cpp
@@ -0,0 +1,66 @@
+#include <regex>
+
+#include "../utils/utils.h"
+
+void part_one() {
+    auto line = get_input_one_line("./input");
+    std::regex w_regex("mul\\((\\d{1,3}),(\\d{1,3})\\)",
+                       std::regex_constants::icase);
+
+    auto words_begin = std::sregex_iterator(line.begin(), line.end(), w_regex);
+
+    auto words_end = std::sregex_iterator();
+
+    std::cout << "Found: " << std::distance(words_begin, words_end)
+              << "mul instrs\n";
+    int total = 0;
+    for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
+        std::smatch match = *i;
+        int first = std::stoi(match[1].str());
+        int second = std::stoi(match[2].str());
+
+        total += first * second;
+        std::cout << "Match: " << match.str() << "\n";
+    }
+    std::cout << "Total: " << total << std::endl;
+}
+
+void part_two() {
+    auto line = get_input_one_line("./input");
+    std::regex w_regex(
+        "mul\\((\\d{1,3}),(\\d{1,3})\\)|(do\\(\\))|(don't\\(\\))",
+        std::regex_constants::icase);
+
+    auto words_begin = std::sregex_iterator(line.begin(), line.end(), w_regex);
+
+    auto words_end = std::sregex_iterator();
+
+    int total = 0;
+    bool enabled = true;
+    for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
+        std::smatch match = *i;
+        std::string cur_match = match.str();
+        if (cur_match == "don't()") {
+            enabled = false;
+        } else if (cur_match == "do()") {
+            enabled = true;
+        }
+
+        if (cur_match.find("mul") != std::string::npos) {
+            int first = std::stoi(match[1].str());
+            int second = std::stoi(match[2].str());
+
+            if (enabled) {
+                total += first * second;
+            }
+        }
+        std::cout << "Match: " << match.str() << "\n";
+    }
+    std::cout << "Total: " << total << std::endl;
+}
+
+int main() {
+    part_one();
+    part_two();
+    return 0;
+}
