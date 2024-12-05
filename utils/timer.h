d9f8c505b45a81f45186a83ff81c723f6decde52
Jacob Latonis <jlatonis@me.com>
2024-12-04 17:26:55 -0700

    Introduce timer for perf baselining

---
+++ utils/timer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+template <typename T>
+struct timer;
+
+template <typename R, typename... Args>
+struct timer<R (*)(Args...)> {
+    using fn = R (*)(Args...);
+
+    static R time(fn func, Args... args) {
+        auto start = std::chrono::high_resolution_clock::now();
+        R retval = func(args...);
+        auto stop = std::chrono::high_resolution_clock::now();
+
+        auto duration =
+            std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+        std::cout << std::format("Func took {} milliseconds", duration.count())
+                  << std::endl;
+
+        return retval;
+    }
+};
--- utils/utils.h
+++ utils/utils.h
@@ -13,6 +13,7 @@
 #include <vector>
 
 #include "node.h"
+#include "timer.h"
 
 std::string get_input_one_line(std::string filepath);
 std::vector<std::string> get_input_all_lines(std::string filepath);
