f5c29ff43a91c389a4f785138249516880fe4b7a
Jacob Latonis <jlatonis@me.com>
2024-12-06 21:48:08 -1000

    better utils

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -1,7 +1,14 @@
 #include "utils.h"
 
-void print_vec(std::vector<int> v) {
+void print_vec_int(std::vector<int> v) {
     for (auto val : v) {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
+}
+
+void print_vec(std::vector<std::string> v) {
+    for (const auto& val : v) {
         std::cout << val << " ";
     }
     std::cout << "\n";
@@ -21,6 +28,31 @@ std::string get_input_one_line(std::string filepath) {
         std::getline(inputFile, line);
     }
     return line;
+}
+
+std::vector<std::vector<std::string>> get_input_blocks(std::string filepath) {
+    std::ifstream inputFile(filepath);
+    std::vector<std::string> v1;
+    std::vector<std::string> v2;
+
+    bool break_encountered = false;
+
+    if (inputFile.is_open()) {
+        for (std::string line; std::getline(inputFile, line);) {
+            if (line == "") {
+                break_encountered = true;
+                continue;
+            }
+
+            if (not break_encountered) {
+                v1.push_back(line);
+            } else {
+                v2.push_back(line);
+            }
+        }
+    }
+
+    return {v1, v2};
 }
 
 std::vector<std::string> get_input_all_lines(std::string filepath) {
--- utils/utils.h
+++ utils/utils.h
@@ -17,6 +17,8 @@
 
 std::string get_input_one_line(std::string filepath);
 std::vector<std::string> get_input_all_lines(std::string filepath);
-void print_vec(std::vector<int>);
+std::vector<std::vector<std::string>> get_input_blocks(std::string filepath);
+void print_vec_int(std::vector<int>);
+void print_vec(std::vector<std::string>);
 void print_vec_hex(std::vector<int>);
 int sv_to_int(std::string_view);
