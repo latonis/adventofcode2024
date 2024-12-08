e12c7b65b52bf26fe74f10ef017ca45de9a998e0
Jacob Latonis <jlatonis@me.com>
2024-12-08 09:23:19 -1000

    day 6 part two solved

--- day06/solution.cpp
+++ day06/solution.cpp
@@ -7,21 +7,116 @@ struct Obstacle {
 };
 
 enum Direction { Up, Down, Left, Right };
+std::set<std::pair<int, int>> scored = std::set<std::pair<int, int>>();
 
 struct Cursor {
     int x;
     int y;
     Direction direction;
+
+    void move() {
+        switch (direction) {
+            case Direction::Up:
+                y -= 1;
+                break;
+            case Direction::Down:
+                y += 1;
+                break;
+            case Direction::Left:
+                x -= 1;
+                break;
+            case Direction::Right:
+                x += 1;
+                break;
+            default:;
+        }
+    }
+
+    void turn_right() {
+        switch (direction) {
+            case Direction::Up:
+                direction = Direction::Right;
+                break;
+            case Direction::Down:
+                direction = Direction::Left;
+                break;
+            case Direction::Left:
+                direction = Direction::Up;
+                break;
+            case Direction::Right:
+                direction = Direction::Down;
+                break;
+            default:;
+        }
+    }
 };
 
-int travel(Cursor sp, std::set<std::pair<int, int>> obstacles, int xn, int yn) {
+int find_loops(Cursor sp, std::set<std::pair<int, int>> obstacles, int xn,
+               int yn, std::set<std::pair<int, int>> positions_visited) {
+    int loops = 0;
+    std::set<std::tuple<int, int, Direction>> positions_visited_l;
+    auto sp_og = sp;
+
+    for (const auto& p : positions_visited) {
+        if (p.first == sp_og.x and p.second == sp_og.y) {
+            continue;
+        }
+
+        obstacles.insert({p.first, p.second});
+
+        while (sp.x >= 0 && sp.x < xn && sp.y < yn && sp.y >= 0) {
+            if (positions_visited_l.contains({sp.x, sp.y, sp.direction})) {
+                loops += 1;
+                break;
+            }
+
+            positions_visited_l.insert({sp.x, sp.y, sp.direction});
+
+        back_to_loop:
+            int p_x = sp.x;
+            int p_y = sp.y;
+
+            switch (sp.direction) {
+                case Direction::Down:
+                    p_y += 1;
+                    break;
+                case Direction::Up:
+                    p_y -= 1;
+                    break;
+                case Direction::Left:
+                    p_x -= 1;
+                    break;
+                case Direction::Right:
+                    p_x += 1;
+                    break;
+                default:;
+            }
+
+            if (obstacles.contains({p_x, p_y})) {
+                sp.turn_right();
+                goto back_to_loop;
+            }
+
+            sp.move();
+        }
+
+        obstacles.erase({p.first, p.second});
+        positions_visited_l.clear();
+        sp = sp_og;
+    }
+    return loops;
+}
+
+std::set<std::pair<int, int>> travel(Cursor sp,
+                                     std::set<std::pair<int, int>> obstacles,
+                                     int xn, int yn, bool check_loop) {
     std::set<std::pair<int, int>> positions_visited;
+    std::set<std::tuple<int, int, Direction>> loops_pls;
+
     while (sp.x >= 0 && sp.x < xn && sp.y < yn && sp.y >= 0) {
         positions_visited.insert({sp.x, sp.y});
-
         int p_x = sp.x;
         int p_y = sp.y;
-
         switch (sp.direction) {
             case Direction::Down:
                 p_y += 1;
@@ -39,43 +134,40 @@ int travel(Cursor sp, std::set<std::pair<int, int>> obstacles, int xn, int yn) {
         }
 
         if (obstacles.contains({p_x, p_y})) {
-            switch (sp.direction) {
-                case Direction::Up:
-                    sp.direction = Direction::Right;
-                    break;
-                case Direction::Down:
-                    sp.direction = Direction::Left;
-                    break;
-                case Direction::Left:
-                    sp.direction = Direction::Up;
-                    break;
-                case Direction::Right:
-                    sp.direction = Direction::Down;
-                    break;
-                default:;
-            }
+            sp.turn_right();
         }
-        switch (sp.direction) {
-            case Direction::Up:
-                sp.y -= 1;
-                break;
-            case Direction::Down:
-                sp.y += 1;
-                break;
-            case Direction::Left:
-                sp.x -= 1;
-                break;
-            case Direction::Right:
-                sp.x += 1;
-                break;
-            default:;
-        }
+
+        sp.move();
     }
 
-    return positions_visited.size();
+    return positions_visited;
 }
 
 int part_one() {
+    std::set<std::pair<int, int>> obstacles;
+    auto lines = get_input_all_lines("./test-input");
+    int yn = lines.size();
+    int xn = lines[0].size();
+    Cursor sp;
+
+    for (int y = 0; y < yn; y++) {
+        for (int x = 0; x < xn; x++) {
+            char c = lines[y][x];
+            if (c == '#') {
+                obstacles.insert({x, y});
+            } else if (c == '^') {
+                sp = Cursor(x, y, Direction::Up);
+            }
+        }
+    }
+
+    std::cout << "Traveled positions: "
+              << travel(sp, obstacles, xn, yn, false).size() << "\n";
+
+    return 0;
+}
+
+int part_two() {
     std::set<std::pair<int, int>> obstacles;
     auto lines = get_input_all_lines("./input");
     int yn = lines.size();
@@ -93,18 +185,10 @@ int part_one() {
         }
     }
 
-    std::cout << "Traveled positions: " << travel(sp, obstacles, xn, yn)
-              << "\n";
+    auto pos_visited = travel(sp, obstacles, xn, yn, false);
 
-    return 0;
-}
-
-int part_two() {
-    auto line = get_input_one_line("./test-input");
-    auto inp =
-        line | std::ranges::views::split(' ') |
-        std::views::transform([](auto v) { return std::string_view(v); }) |
-        std::ranges::to<std::vector>();
+    std::cout << "Traveled positions: "
+              << find_loops(sp, obstacles, xn, yn, pos_visited) << "\n";
 
     return 0;
 }
