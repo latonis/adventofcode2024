e884b891a1c54cbc3a4eaf04aa7e8b828d3d35aa
Jacob Latonis <jlatonis@me.com>
2024-12-07 21:55:53 -1000

    day 05 part 2 solved

--- day05/solution.cpp
+++ day05/solution.cpp
@@ -27,6 +27,31 @@ bool valid_layout(std::vector<int> page,
     return true;
 }
 
+std::vector<int> fix_layout(std::vector<int> page,
+                            std::unordered_map<int, std::set<int>> order_map) {
+    while (not valid_layout(page, order_map)) {
+        for (auto o : order_map) {
+            int i1 = index_of(page, o.first);
+            for (auto const& s : o.second) {
+                int i2 = index_of(page, s);
+                if (i1 == -1 || i2 == -1) {
+                    continue;
+                }
+
+                if (i2 > i1) {
+                    while (i2 > i1) {
+                        std::swap(page[i2], page[i2 - 1]);
+                        i2 = index_of(page, s);
+                        i1 = index_of(page, o.first);
+                    }
+                }
+            }
+        }
+    }
+
+    return page;
+}
+
 int part_one() {
     auto blocks = get_input_blocks("./input");
     auto rules = blocks[0];
@@ -71,17 +96,53 @@ int part_one() {
 }
 
 int part_two() {
-    auto line = get_input_one_line("./test-input");
-    auto inp =
-        line | std::ranges::views::split(' ') |
-        std::views::transform([](auto v) { return std::string_view(v); }) |
-        std::ranges::to<std::vector>();
+    auto blocks = get_input_blocks("./input");
+    auto rules = blocks[0];
+    auto pages_s = blocks[1];
+    std::vector<std::vector<int>> pages;
+    for (const auto& p : pages_s) {
+        auto to_push = p | std::views::split(',') |
+                       std::views::transform([](auto s) {
+                           int i = 0;
+                           std::from_chars(s.data(), s.data() + s.size(), i);
+                           return i;
+                       }) |
+                       std::ranges::to<std::vector>();
 
+        pages.push_back(to_push);
+    }
+    std::unordered_map<int, std::set<int>> order_map;
+
+    for (const auto& r : rules) {
+        auto order = r | std::views::split('|') |
+                     std::views::transform([](auto v) {
+                         int i = 0;
+                         std::from_chars(v.data(), v.data() + v.size(), i);
+                         return i;
+                     }) |
+                     std::ranges::to<std::vector>();
+
+        if (order_map.contains(order[1])) {
+            order_map.find(order[1])->second.insert(order[0]);
+
+        } else {
+            order_map.insert({order[1], {order[0]}});
+        }
+    }
+
+    int c = 0;
+    for (const auto& page : pages) {
+        if (not valid_layout(page, order_map)) {
+            std::vector<int> n_vec = fix_layout(page, order_map);
+            c += n_vec[n_vec.size() / 2];
+        }
+    }
+    std::cout << "Total: " << c << std::endl;
     return 0;
 }
 
 int main() {
     auto part1 = timer<decltype(&part_one)>::time(part_one);
-    // auto part2 = timer<decltype(&part_two)>::time(part_two);
+    auto part2 = timer<decltype(&part_two)>::time(part_two);
     return 0;
 }
